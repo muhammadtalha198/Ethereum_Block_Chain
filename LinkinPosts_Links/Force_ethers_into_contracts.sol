6e9bf61666a12461d35aa7e5458f63b840bfa1ba
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2023-01-12 12:35:54 +0500

    Create Force_ethers_into_contracts.sol

---
+++ LinkinPosts_Links/Force_ethers_into_contracts.sol
@@ -0,0 +1,2 @@
+https://medium.com/@alexsherbuck/two-ways-to-force-ether-into-a-contract-1543c1311c56
+https://aniketengg.medium.com/ways-to-send-eth-to-a-contract-having-throw-in-fallback-function-41765db796de
