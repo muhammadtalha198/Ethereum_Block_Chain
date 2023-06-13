2fcebdb77ae993020d8cd0868650b966a3e48002
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2023-06-13 12:11:59 +0500

    Create SwapTokensForTokens.sol

---
+++ UniswapContracts/SwapTokensForTokens.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+interface IUniswapV2Router02{
+    function factory() external pure returns (address);
+    function WETH() external pure returns (address);
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB, uint liquidity);
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+}
+
+contract SwapTokenContract{
+
+    
+
+}
