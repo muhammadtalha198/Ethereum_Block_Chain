2f2de6c83bb54fefc528c5e957e3dc39b5835687
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2023-07-10 12:50:52 +0500

    Update 155_minting_contract.sol

--- NFT_1155_contract/155_minting_contract.sol
+++ NFT_1155_contract/155_minting_contract.sol
@@ -1,1 +1,70 @@
-....
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract NFT1155 is ERC1155, Ownable, Pausable {
+    
+  string public name;
+  string public symbol;
+
+    uint256 public constant Rock = 1;
+    uint256 public constant Paper = 2;
+    uint256 public constant Scissors = 3;
+
+  constructor() ERC1155("https://ipfs.io/ipfs/QmcUYQsr6C5y7mq6HJsLj99PSNgajdpDdBXBhVm7u9oSHB/{id}.json") {
+    name = "HashItems";
+    symbol = "HASHITEMS";
+
+        _mint(msg.sender, Rock, 1, "");
+        _mint(msg.sender, Paper, 1, "");
+        _mint(msg.sender, Scissors, 1, "");
+  }
+
+  function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+   function uri(uint256 _tokenid) override public pure returns (string memory) {
+        return string(
+            abi.encodePacked(
+                "https://ipfs.io/ipfs/QmcUYQsr6C5y7mq6HJsLj99PSNgajdpDdBXBhVm7u9oSHB/",
+                Strings.toString(_tokenid),".json"
+            )
+        );
+    }
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+}
+    
