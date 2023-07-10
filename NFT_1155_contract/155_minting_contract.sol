fbe6d6fc07360f5cc185a9ad1922a70f2b0967bf
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2023-07-10 13:25:33 +0500

    Update 155_minting_contract.sol

--- NFT_1155_contract/155_minting_contract.sol
+++ NFT_1155_contract/155_minting_contract.sol
@@ -1,30 +1,19 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
-pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Strings.sol"; 
 
-contract NFT1155 is ERC1155, Ownable, Pausable {
-    
-  string public name;
-  string public symbol;
 
-    uint256 public constant Rock = 1;
-    uint256 public constant Paper = 2;
-    uint256 public constant Scissors = 3;
+contract MyToken is ERC1155, Ownable {
+    constructor() ERC1155("") {}
 
-  constructor() ERC1155("https://ipfs.io/ipfs/QmcUYQsr6C5y7mq6HJsLj99PSNgajdpDdBXBhVm7u9oSHB/{id}.json") {
-    name = "HashItems";
-    symbol = "HASHITEMS";
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
 
-        _mint(msg.sender, Rock, 1, "");
-        _mint(msg.sender, Paper, 1, "");
-        _mint(msg.sender, Scissors, 1, "");
-  }
-
-  function mint(address account, uint256 id, uint256 amount, bytes memory data)
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
         public
         onlyOwner
     {
@@ -38,33 +27,11 @@ contract NFT1155 is ERC1155, Ownable, Pausable {
         _mintBatch(to, ids, amounts, data);
     }
 
-    function setURI(string memory newuri) public onlyOwner {
-        _setURI(newuri);
-    }
-
-   function uri(uint256 _tokenid) override public pure returns (string memory) {
+    function tokenURI(uint256 _tokenid) public view returns (string memory) {
+        
+        string memory currentBaseURI = uri(_tokenid);
         return string(
-            abi.encodePacked(
-                "https://ipfs.io/ipfs/QmcUYQsr6C5y7mq6HJsLj99PSNgajdpDdBXBhVm7u9oSHB/",
-                Strings.toString(_tokenid),".json"
-            )
-        );
-    }
-    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        internal
-        whenNotPaused
-        override
-    {
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
-    }
-
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
+            abi.encodePacked(currentBaseURI,Strings.toString(_tokenid),".json"));
     }
 
 }
-    
