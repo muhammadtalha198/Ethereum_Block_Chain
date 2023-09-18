7250da505f8bf1896b60965deddcab21f2882baa
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2023-09-18 11:18:12 +0500

    Update ERC1155_Owner_UploadAll.sol

--- IFERS_NP_MarketPlace/ERC1155_Owner_UploadAll.sol
+++ IFERS_NP_MarketPlace/ERC1155_Owner_UploadAll.sol
@@ -1,1 +1,76 @@
 
+/*
+
+    In case owner is minting by him self then sale them 
+    The ipfs link : https://ipfs.io/ipfs/QmTqdUbVkaNfie3YvJK4J7j2nErtMCkxwEfK7KzedFSpMs/{id}.json
+    used to set in setUri function will be : https://ipfs.io/ipfs/QmTqdUbVkaNfie3YvJK4J7j2nErtMCkxwEfK7KzedFSpMs/
+
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155SupplyUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+
+contract MyToken is Initializable, ERC1155Upgradeable, OwnableUpgradeable, PausableUpgradeable, ERC1155BurnableUpgradeable, ERC1155SupplyUpgradeable, UUPSUpgradeable {
+    
+    CountersUpgradeable.Counter private _tokenIdCounter;
+    
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC1155_init("https://ipfs.io/ipfs/QmTqdUbVkaNfie3YvJK4J7j2nErtMCkxwEfK7KzedFSpMs/");
+        __Ownable_init();
+        __Pausable_init();
+        __ERC1155Burnable_init();
+        __ERC1155Supply_init();
+        __UUPSUpgradeable_init();
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data) public onlyOwner {
+         uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) public onlyOwner{
+        _mintBatch(to, ids, amounts, data);
+    }
+    
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        internal
+        whenNotPaused
+        override(ERC1155Upgradeable, ERC1155SupplyUpgradeable)
+    {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+}
