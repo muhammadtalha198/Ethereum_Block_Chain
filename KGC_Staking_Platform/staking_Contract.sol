874e055f6b8e3d176c8f7ab0bf195122848c872f
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2024-01-22 22:19:58 +0500

    Update staking_Contract.sol

--- KGC_Staking_Platform/staking_Contract.sol
+++ KGC_Staking_Platform/staking_Contract.sol
@@ -1,1 +1,118 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "hardhat/console.sol";
+
+
+interface IBEP20 {        
+    
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function transferFrom(address sender,address recipient,uint256 amount) external returns (bool);
+}
+
+contract MyContract is Initializable, PausableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    
+    
+    IBEP20 public kgcToken;
+    IBEP20 public usdtToken;
+
+
+    struct Stake {
+        uint256 stakeamount;
+        uint256 totalReward;
+        uint256 noOfRefferals;
+    }
+
+    struct ReferalPerson{
+        address referalAddress;
+        uint256 referalLevel;
+    }
+    
+    uint256 public registrerationFee;
+    uint256 public minimumAmount;
+    uint256 public maximumAmount;
+    uint256 public totalDuration;
+    uint256 public rewardLevelPercentages;
+
+    /*
+     level 1 =        50 %
+     level 2 =        10  %    
+     level 3 to 5 =    5 %
+     level 6 to 10 =   3 %
+     level 11 to 30 =  2 %
+     level 31 to 50  = 1 %
+    
+    */
+
+    mapping(address => Stake) public stakeInfo;
+    mapping(address => bool) public userRegistered;
+    mapping(uint256 => ReferalPerson) public referalPerson;
+
+
+    event Registered(address registeredUser, uint256 fee);
+
+    
+    
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address initialOwner, address _kgcToken, address _usdtToken) initializer public {
+        __Pausable_init();
+        __Ownable_init(initialOwner);
+        __UUPSUpgradeable_init();
+
+        kgcToken = IBEP20(_kgcToken);
+        usdtToken = IBEP20(_usdtToken);
+        registrerationFee = 5 * 1e18;
+        minimumAmount = 2 * 1e18;
+        maximumAmount = 50 * 1e18;
+        totalDuration = 500 days;
+    }
+
+
+
+
+    function registerUser(uint256 _fee) external {
+        
+        require (_fee >= registrerationFee, "Invalid fee.");
+
+        userRegistered[msg.sender] = true;
+        usdtToken.transferFrom(msg.sender, owner(), _fee);
+
+        emit Registered(msg.sender,_fee);
+    }
+
+    function stakeTokens(uint256 _amount) external view {
+        
+        require(_amount >= minimumAmount && _amount <= maximumAmount, "invalid amount!");
+        require(userRegistered[msg.sender], "Plaese register!");
+
+
+
+
+    }
+
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+}
 
