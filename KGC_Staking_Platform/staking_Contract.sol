0fb6ffcd91b72fc37f7b5b28b2430838289d4a55
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2024-01-29 13:22:19 +0500

    Update staking_Contract.sol

--- KGC_Staking_Platform/staking_Contract.sol
+++ KGC_Staking_Platform/staking_Contract.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-
+import "hardhat/console.sol";
 
 
 interface IBEP20 {        
@@ -14,6 +14,9 @@ interface IBEP20 {
     function transfer(address recipient, uint256 amount) external returns (bool);
     function allowance(address owner, address spender) external view returns (uint256);
     function transferFrom(address sender,address recipient,uint256 amount) external returns (bool);
+}
+interface IPancakeRouter01 {
+    function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
 }
 
  /*
@@ -30,77 +33,62 @@ contract MyContract is Initializable, PausableUpgradeable, OwnableUpgradeable, U
     
     
     IBEP20 public kgcToken;
-    IBEP20 public usdtToken;
+    IBEP20 public usdcToken;
+    IPancakeRouter01 public pancakeRouter;
 
 
-    struct Stake {
-        uint256 stakeAmount;
-        uint256 totalReward;
-    }
-
-
-
-
-    
     uint256 public registrerationFee;
     uint256 public minimumAmount;
     uint256 public maximumAmount;
     uint256 public totalDuration;
     uint256[] public rewardLevelPercentages;
 
-
-
-
-    
-    
-    
-    mapping(address => Stake) public stakeInfo;
-
-
-    event Registered(address registeredUser, uint256 fee);
-    event Registered(address regissteredUser, address referalPerson, uint256 _fee);
-
-    
-    
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize(address initialOwner, address _kgcToken, address _usdtToken) initializer public {
-        __Pausable_init();
-        __Ownable_init(initialOwner);
-        __UUPSUpgradeable_init();
-
-        kgcToken = IBEP20(_kgcToken);
-        usdtToken = IBEP20(_usdtToken);
-        registrerationFee = 5 * 1e18;
-        minimumAmount = 2 * 1e18;
-        maximumAmount = 50 * 1e18;
-        totalDuration = 500 days;
-
-        setRewardPercentages();
-    }
+    address usdcAddress;
+    address kgcAddress;
 
 
     struct UserRegistered{
         bool haveReferal;
         bool registered;
         uint256 noOfreferals;
-        mapping(uint256 => ReferalInfo) hasReferal;
     }
-        mapping(address => mapping(uint256 => address)) public  hasReferal1;
-
-    struct ReferalInfo{
-        address referalPerson;
-        mapping(address => uint256) referalLevel;
-    }
-
-    mapping(address => mapping(uint256 =>mapping (address => uint256))) public  referalLevel1;
-
-    mapping(address => UserRegistered) public userRegistered;
     
+    struct Stake {
+        uint256 stakeAmount;
+        uint256 totalStakedReward;
+        uint256 totalReferalReward;
+    }
+   
+   
+    mapping(address => Stake) public stakeInfo;
+    mapping(address => UserRegistered) public userRegistered;
+    mapping(address => mapping(uint256 => address)) public referalPerson;
+    mapping(address => mapping(uint256 => mapping(address => uint256))) public referalPersonLevel;
+    
+    event Registered(address regissteredUser, address referalPerson, uint256 _fee);
 
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address initialOwner, address _kgcToken, address _usdcToken, address _pancakeRouter) initializer public {
+        __Pausable_init();
+        __Ownable_init(initialOwner);
+        __UUPSUpgradeable_init();
+
+        kgcToken = IBEP20(_kgcToken);
+        usdcToken = IBEP20(_usdcToken);
+        registrerationFee = 5 * 1e18;
+        minimumAmount = 2 * 1e18;
+        maximumAmount = 50 * 1e18;
+        totalDuration = 500 days;
+        usdcAddress = _usdcToken;
+        kgcAddress = _kgcToken;
+        pancakeRouter = IPancakeRouter01(_pancakeRouter);
+
+        setRewardPercentages();
+    }
     function registerUser(uint256 _fee, address referalAddress) external {
         
         require(referalAddress != msg.sender && referalAddress != address(0), "invalid referal Address!");
@@ -113,67 +101,74 @@ contract MyContract is Initializable, PausableUpgradeable, OwnableUpgradeable, U
         if(userRegistered[msg.sender].haveReferal){
 
             if(!userRegistered[referalAddress].haveReferal){
+
                 userRegistered[msg.sender].noOfreferals = 1;
-                userRegistered[msg.sender].hasReferal[0].referalPerson = referalAddress;
-                userRegistered[msg.sender].hasReferal[0].referalLevel[referalAddress] = 1;
-                
+                referalPerson[msg.sender][0] = referalAddress;
+                referalPersonLevel[msg.sender][0][referalAddress] = 1;
+
             }else{
-                uint256 previousReferal = userRegistered[msg.sender].noOfreferals;
+                
+                uint256 previousReferal = userRegistered[referalAddress].noOfreferals;
                
                 for(uint256 i=0; i < previousReferal; i++){
-                    userRegistered[msg.sender].hasReferal[i].referalPerson = userRegistered[referalAddress].hasReferal[i].referalPerson;
-                    userRegistered[msg.sender].hasReferal[i].referalLevel[userRegistered[msg.sender].hasReferal[i].referalPerson] =  
-                    (userRegistered[referalAddress].hasReferal[i].referalLevel[userRegistered[referalAddress].hasReferal[i].referalPerson] + 1);
+                    
+                    referalPerson[msg.sender][i] = referalPerson[referalAddress][i];
+                    referalPersonLevel[msg.sender][i][referalPerson[msg.sender][i]] = 
+                    (referalPersonLevel[referalAddress][i][referalPerson[referalAddress][i]] + 1);
                     userRegistered[msg.sender].noOfreferals ++;
-                }
 
-                userRegistered[msg.sender].hasReferal[previousReferal].referalPerson = referalAddress;
-                userRegistered[msg.sender].hasReferal[previousReferal].referalLevel[referalAddress] = 1;
+                }
+                    referalPerson[msg.sender][previousReferal] = referalAddress;
+                    referalPersonLevel[msg.sender][previousReferal][referalAddress] =  1;
+                    userRegistered[msg.sender].noOfreferals++;
             }
         }
 
     }
 
-// hasReferal1[msg.sender][0] = referalAddress;
-//                 referalLevel1[msg.sender][0][referalAddress] = 1;
-
-
-// hasReferal1[msg.sender][i] = hasReferal1[referalAddress][i];
-//                     referalLevel1[msg.sender][i][hasReferal1[msg.sender][i]] = 
-                    
 
 
 
-
-
-
-
-        // emit Registered(msg.sender,_fee);
 
    
-   
-   
-   
-   
-   
-   
-   
-   
-    // function stakeTokens(uint256 _amount, address referalAddress) external  {
+    function stakeTokens(uint256 _amount) external  {
         
-    //     require(_amount >= minimumAmount && _amount <= maximumAmount, "invalid amount!");
-    //     require(userRegistered[msg.sender].registered, "Plaese register!");
+        require(_amount >= minimumAmount && _amount <= maximumAmount, "invalid amount!");
+        require(userRegistered[msg.sender].registered, "Plaese register!");
 
-    //     stakeInfo[msg.sender].stakeAmount = _amount;
-    //     stakeInfo[msg.sender].referalPerson = referalAddress;
-    //     stakeInfo[msg.sender].noOfRefferals += 1;
+        uint256 kgcTokenAmount = getKGCAmount(_amount);
 
+        require(kgcToken.balanceOf(msg.sender) >= kgcTokenAmount,"insufficient Kgc balancce.");
 
+        stakeInfo[msg.sender].stakeAmount = _amount;
 
+    }
 
+    function getKGCAmount(uint256 _usdcAmount) public view returns(uint256){
+        
+        address[] memory pathTogetKGC = new address[](2);
+        pathTogetKGC[0] = usdcAddress;
+        pathTogetKGC[1] = kgcAddress;
 
+        uint256[] memory _kgcAmount;
+        _kgcAmount = pancakeRouter.getAmountsOut(_usdcAmount,pathTogetKGC);
 
-    // }
+        return _kgcAmount[1];
+
+    } 
+
+    // how much busd aginst one gen.
+    function getKGCPrice(uint256 _kgcAmount) public  view returns(uint256){
+        
+        address[] memory pathTogetKGCPrice = new address[](2);
+        pathTogetKGCPrice[0] = kgcAddress;
+        pathTogetKGCPrice[1] = usdcAddress;
+
+        uint256[] memory _kgcPrice;
+        _kgcPrice = pancakeRouter.getAmountsOut(_kgcAmount,pathTogetKGCPrice);
+
+        return _kgcPrice[1];
+    }
 
 
 
@@ -205,3 +200,19 @@ contract MyContract is Initializable, PausableUpgradeable, OwnableUpgradeable, U
         override
     {}
 }
+
+// 0x0000000000000000000000000000000000000000
+
+// 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+// 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+// 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
+// 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
+
+
+// org1 = 0xdD870fA1b7C4700F2BD7f44238821C26f7392148
+// org2 = 0x583031D1113aD414F02576BD6afaBfb302140225
+// org3 = 0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB
+// fiscl = 0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C
+
+//  mint :0xf8e81D47203A594245E36C48e151709F0C19fBe8
+// mrkt = 0x7EF2e0048f5bAeDe046f6BF797943daF4ED8CB47
