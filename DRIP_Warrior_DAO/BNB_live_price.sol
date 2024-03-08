e199f62986a93b2e48f628e6a8facb0405d31e22
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2024-03-08 16:57:20 +0500

    Update BNB_live_price.sol

--- DRIP_Warrior_DAO/BNB_live_price.sol
+++ DRIP_Warrior_DAO/BNB_live_price.sol
@@ -1,1 +1,17 @@
+pragma solidity ^0.8.0;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract BNBPriceConsumer {
+    AggregatorV3Interface internal priceFeed;
+
+    constructor() {
+        // Chainlink BNB/USD price feed address on Binance Smart Chain Mainnet
+        priceFeed = AggregatorV3Interface(0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE);
+    }
+
+    function getBNBPrice() public view returns (int) {
+        (, int price, , , ) = priceFeed.latestRoundData();
+        return price;
+    }
+}
