fe94639d72e2f7b8c1b639532924ad861b94633a
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2024-04-08 11:16:59 +0500

    Create ReadMe.sol

---
+++ KGC_Staking_Platform/Final_Contracts/ReadyTodeploy/ReadMe.sol
@@ -0,0 +1,16 @@
+Mainnet contract addresses!
+Main KGC address https://bscscan.com/address/0xbd49fac536b73f8dacb69995b96699e116decb39
+
+Proxy KGC Address 
+https://bscscan.com/address/0x5C76F44a5B2d6B085486Fd645A7982b3928F80bC
+
+
+Main Staking Address 
+https://bscscan.com/address/0x3a5a4fcc3187302b67b5b0f0ddc1bd36f7b6a6aa
+
+Proxy Staking Address
+https://bscscan.com/address/0xaBcA6059B5fd09D3960caf5CF05636F21e4Ba4DD
+
+
+USDT 
+https://bscscan.com/address/0x55d398326f99059ff775485246999027b3197955
