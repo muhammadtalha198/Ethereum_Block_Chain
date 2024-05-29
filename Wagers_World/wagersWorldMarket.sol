bce46977179d005691a4ad5cea5f83cb860fafe2
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2024-05-29 14:42:16 +0500

    Update wagersWorldMarket.sol

--- Wagers_World/wagersWorldMarket.sol
+++ Wagers_World/wagersWorldMarket.sol
@@ -23,11 +23,14 @@ contract Market is Ownable {
         uint256 totalBets;
         uint256[2] YesOrNo;
         uint256 totalAmount;
+        uint256[2] initialPrice;
+        uint256 totalBetsOnYes;
+        uint256 totalBetsOnNo;
     }
 
     struct UserInfo{
         bool bet;
-        uint256 beton;
+        uint256 betOn;
         uint256 betAmount;
     }
 
@@ -55,7 +58,10 @@ contract Market is Ownable {
 
             marketInfo[address(this)].endTime = _endTime;
             marketInfo[address(this)].marketOpen = true ;
+            marketInfo[address(this)].initialPrice[0] = 500000000000000000;
+            marketInfo[address(this)].initialPrice[1] = 500000000000000000;
             usdcToken = IERC20(_usdcToken);
+            
 
 
     }
@@ -74,166 +80,166 @@ contract Market is Ownable {
         bool success = usdcToken.transferFrom(msg.sender, address(this), _amount);
         require(success, "Transfer failed");
 
-        emit BuyEvent(msg.sender, _amount);
+        // emit BuyEvent(msg.sender, _amount);
     }
 
-    function SELL(uint256 outcomeIndex, uint256 _amount) external {
+//     function SELL(uint256 outcomeIndex, uint256 _amount) external {
         
-        require(state == MarketState.Open, "Market is not open");
-        require(outcomeIndex < outcomes.length, "Invalid outcome index");
-        require(block.timestamp < endTime, "Market has ended");
-        require(_amount > 0, "Sell amount must be greater than 0");
+//         require(state == MarketState.Open, "Market is not open");
+//         require(outcomeIndex < outcomes.length, "Invalid outcome index");
+//         require(block.timestamp < endTime, "Market has ended");
+//         require(_amount > 0, "Sell amount must be greater than 0");
         
-        require(userBalances[msg.sender][outcomeIndex] >= _amount,"Insufficient balance");
+//         require(userBalances[msg.sender][outcomeIndex] >= _amount,"Insufficient balance");
 
-        // Calculate the amount to return to the user based on their share of the outcome
-        uint256 returnAmount = (_amount * outcomes[outcomeIndex].totalBets) / outcomes[outcomeIndex].totalSharess;
+//         // Calculate the amount to return to the user based on their share of the outcome
+//         uint256 returnAmount = (_amount * outcomes[outcomeIndex].totalBets) / outcomes[outcomeIndex].totalSharess;
 
-        // Update user balance and total bets for the outcome
-        userBalances[msg.sender][outcomeIndex] -=  _amount;
-        outcomes[outcomeIndex].totalBets -= _amount;
-        outcomes[outcomeIndex].totalSharess -= returnAmount;
+//         // Update user balance and total bets for the outcome
+//         userBalances[msg.sender][outcomeIndex] -=  _amount;
+//         outcomes[outcomeIndex].totalBets -= _amount;
+//         outcomes[outcomeIndex].totalSharess -= returnAmount;
 
-        require(usdcToken.transfer(msg.sender, returnAmount));
+//         require(usdcToken.transfer(msg.sender, returnAmount));
 
-        emit SellEvent(msg.sender, outcomeIndex, _amount, returnAmount);
-    }
+//         emit SellEvent(msg.sender, outcomeIndex, _amount, returnAmount);
+//     }
 
-     // Function to resolve the market and determine the winning outcome
-    function resolveMarket(uint256 winningOutcomeIndex) external onlyOwner {
+//      // Function to resolve the market and determine the winning outcome
+//     function resolveMarket(uint256 winningOutcomeIndex) external onlyOwner {
         
-        require(state == MarketState.Open, "Market is not open");
-        require(block.timestamp > endTime, "Market has not ended");
-        require(winningOutcomeIndex < outcomes.length, "Invalid outcome index");
+//         require(state == MarketState.Open, "Market is not open");
+//         require(block.timestamp > endTime, "Market has not ended");
+//         require(winningOutcomeIndex < outcomes.length, "Invalid outcome index");
 
-        // Update the state of the market to Resolved
-        state = MarketState.Resolved;
-        result = winningOutcomeIndex;
+//         // Update the state of the market to Resolved
+//         state = MarketState.Resolved;
+//         result = winningOutcomeIndex;
 
-        // Emit the MarketResolved event
-        emit MarketResolved(winningOutcomeIndex);
-    }
+//         // Emit the MarketResolved event
+//         emit MarketResolved(winningOutcomeIndex);
+//     }
 
-    function _userShareAmount(address _user, uint256 outcomeIndex)public view returns (uint256){
+//     function _userShareAmount(address _user, uint256 outcomeIndex)public view returns (uint256){
 
-        uint256 userSharePercentage = (userBalances[_user][outcomeIndex] *100) / outcomes[outcomeIndex].totalBets;
+//         uint256 userSharePercentage = (userBalances[_user][outcomeIndex] *100) / outcomes[outcomeIndex].totalBets;
 
-           return userSharePercentage;
-    }
+//            return userSharePercentage;
+//     }
 
-    function withdrawWinnings() external {
+//     function withdrawWinnings() external {
         
-        uint256 outcomeIndex = result ;
+//         uint256 outcomeIndex = result ;
         
-        require(state == MarketState.Resolved, "Market is not resolved");
-        require(outcomeIndex < outcomes.length, "Invalid outcome index");
-        require(userBalances[msg.sender][outcomeIndex] > 0,"You are not a winner");
+//         require(state == MarketState.Resolved, "Market is not resolved");
+//         require(outcomeIndex < outcomes.length, "Invalid outcome index");
+//         require(userBalances[msg.sender][outcomeIndex] > 0,"You are not a winner");
          
-        if (outcomes[0].totalBets == 0 || outcomes[1].totalBets == 0 ) {
+//         if (outcomes[0].totalBets == 0 || outcomes[1].totalBets == 0 ) {
             
-            uint256 userShare = userBalances[msg.sender][outcomeIndex];
-            userBalances[msg.sender][outcomeIndex] = 0;
+//             uint256 userShare = userBalances[msg.sender][outcomeIndex];
+//             userBalances[msg.sender][outcomeIndex] = 0;
 
-            // Transfer the user's winnings
-            require(usdcToken.transfer(msg.sender, userShare), "Withdraw failed");
+//             // Transfer the user's winnings
+//             require(usdcToken.transfer(msg.sender, userShare), "Withdraw failed");
 
-        }else {
+//         }else {
             
-            uint256 abc;
+//             uint256 abc;
             
-            if(0 == outcomeIndex){
+//             if(0 == outcomeIndex){
 
-                admin = outcomes[1].totalBets * 10 / 100;
-                abc = (outcomes[1].totalBets - admin ) * _userShareAmount(msg.sender, outcomeIndex) / 100;
+//                 admin = outcomes[1].totalBets * 10 / 100;
+//                 abc = (outcomes[1].totalBets - admin ) * _userShareAmount(msg.sender, outcomeIndex) / 100;
 
-            }else if (1 == outcomeIndex){
+//             }else if (1 == outcomeIndex){
 
-                admin = outcomes[0].totalBets * 10 / 100;
-                abc = (outcomes[0].totalBets - admin ) * _userShareAmount(msg.sender, outcomeIndex) / 100;
-            }
+//                 admin = outcomes[0].totalBets * 10 / 100;
+//                 abc = (outcomes[0].totalBets - admin ) * _userShareAmount(msg.sender, outcomeIndex) / 100;
+//             }
 
-            // Transfer the user's winnings
-            require(usdcToken.transfer(msg.sender, abc+userBalances[msg.sender][outcomeIndex]), "Withdraw failed");
+//             // Transfer the user's winnings
+//             require(usdcToken.transfer(msg.sender, abc+userBalances[msg.sender][outcomeIndex]), "Withdraw failed");
         
-        }    
+//         }    
         
-        // Reset the user's balance for the outcome to prevent reentrant attacks
-        userBalances[msg.sender][outcomeIndex] = 0;
+//         // Reset the user's balance for the outcome to prevent reentrant attacks
+//         userBalances[msg.sender][outcomeIndex] = 0;
         
-        emit WithdrawWinner(outcomeIndex);
-    }
+//         emit WithdrawWinner(outcomeIndex);
+//     }
 
-    function getAdminAmount() external returns (uint256 adminAmount) {
+//     function getAdminAmount() external returns (uint256 adminAmount) {
     
-        uint256 outcomeIndex = result ;
+//         uint256 outcomeIndex = result ;
     
-        require(state == MarketState.Resolved, "Market is not resolved");
-        require(outcomeIndex < outcomes.length, "Invalid outcome index");
+//         require(state == MarketState.Resolved, "Market is not resolved");
+//         require(outcomeIndex < outcomes.length, "Invalid outcome index");
 
-        // Calculate the admin amount based on the winning outcome
-        if (outcomeIndex == 0) {
+//         // Calculate the admin amount based on the winning outcome
+//         if (outcomeIndex == 0) {
            
-            adminAmount = outcomes[1].totalBets * 10 / 100;
+//             adminAmount = outcomes[1].totalBets * 10 / 100;
 
-        } else if (outcomeIndex == 1) {
-            adminAmount = outcomes[0].totalBets * 10 / 100;
+//         } else if (outcomeIndex == 1) {
+//             adminAmount = outcomes[0].totalBets * 10 / 100;
 
-        } else {
-            adminAmount = 0; // Invalid outcome index
-        }
+//         } else {
+//             adminAmount = 0; // Invalid outcome index
+//         }
 
-        // Transfer the admin amount to the admin
-        if (adminAmount > 0) {
-            require(usdcToken.transfer(owner(), adminAmount), "Admin transfer failed");
-        }
+//         // Transfer the admin amount to the admin
+//         if (adminAmount > 0) {
+//             require(usdcToken.transfer(owner(), adminAmount), "Admin transfer failed");
+//         }
     
-    return adminAmount;
+//     return adminAmount;
     
-}
+// }
 
-    function RemainingTokens() external onlyOwner {
+//     function RemainingTokens() external onlyOwner {
         
-        uint256 remainingBalance = usdcToken.balanceOf(address(this));
+//         uint256 remainingBalance = usdcToken.balanceOf(address(this));
         
-        require(remainingBalance >0, "No balance in the market");
+//         require(remainingBalance >0, "No balance in the market");
         
-        // Transfer the remaining tokens to the owner
-        require(usdcToken.transfer(owner(), remainingBalance), "Transfer failed");
+//         // Transfer the remaining tokens to the owner
+//         require(usdcToken.transfer(owner(), remainingBalance), "Transfer failed");
        
-        emit RemainingTransfer(owner(),remainingBalance );
-    }
+//         emit RemainingTransfer(owner(),remainingBalance );
+//     }
 
-    function getTotalBetsCombined() external view returns (uint256) {
+//     function getTotalBetsCombined() external view returns (uint256) {
         
-        uint256 totalCombinedBets = 0;
+//         uint256 totalCombinedBets = 0;
         
-        for (uint256 i = 0; i < outcomes.length; i++) {
-            totalCombinedBets = totalCombinedBets + (outcomes[i].totalBets);
-        }
+//         for (uint256 i = 0; i < outcomes.length; i++) {
+//             totalCombinedBets = totalCombinedBets + (outcomes[i].totalBets);
+//         }
         
-        return totalCombinedBets;
-    }
+//         return totalCombinedBets;
+//     }
 
-    function resultPercentage() external view returns (uint256 yesPercentage , uint256 noPercentage ) {
+//     function resultPercentage() external view returns (uint256 yesPercentage , uint256 noPercentage ) {
        
-        uint256 totalBets = outcomes[0].totalBets + outcomes[1].totalBets;  
+//         uint256 totalBets = outcomes[0].totalBets + outcomes[1].totalBets;  
         
-        if (totalBets == 0) {
-            return (0, 0);
-        }
+//         if (totalBets == 0) {
+//             return (0, 0);
+//         }
         
-        yesPercentage = (outcomes[1].totalBets * 100) / totalBets;
-        noPercentage = (outcomes[0].totalBets * 100) / totalBets;
-    }
+//         yesPercentage = (outcomes[1].totalBets * 100) / totalBets;
+//         noPercentage = (outcomes[0].totalBets * 100) / totalBets;
+//     }
 
-    // Function to get information about a specific outcome
-    function getOutcomeInfo(uint256 index)external view returns (string memory description,uint256 totalBets,uint256 totalSharess){
+//     // Function to get information about a specific outcome
+//     function getOutcomeInfo(uint256 index)external view returns (string memory description,uint256 totalBets,uint256 totalSharess){
         
-        require(index < outcomes.length, "Invalid outcome index");
-        OutcomeInfo memory outcome = outcomes[index];
+//         require(index < outcomes.length, "Invalid outcome index");
+//         OutcomeInfo memory outcome = outcomes[index];
         
-        return (outcome.description, outcome.totalBets, outcome.totalSharess);
-    }
+//         return (outcome.description, outcome.totalBets, outcome.totalSharess);
+//     }
 
 
 }
